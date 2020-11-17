76ef193c60e00170d7697d2fc7326e42594c7620
moella <moella0@gmail.com>
2020-11-17 11:48:21 +0100

    navbar toegevoegd

--- Applicatie/index.php
+++ Applicatie/index.php
@@ -1,1 +1,16 @@
 <?php include("Layout/Header.php"); ?>
+
+<nav>
+    <div class="links">
+      <i class="fas fa-apple-alt"></i>
+      <p>Full stack</p>
+    </div>
+    <div class="rechts">
+    <ul>
+      <li>
+        <a class="active" href="index.php">Home</a> |  
+        
+      </li>
+    </ul>
+    </div>
+  </nav>
