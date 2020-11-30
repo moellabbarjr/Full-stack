e0f9a02bdd3ee8e21ce4e4099862c19de3d69fb1
Max Ridderikhoff <maxridderikhoff@outlook.com>
2020-11-30 10:59:56 +0100

    Private folder added, login page Front-End added and css for login page

--- Applicatie/index.php
+++ Applicatie/index.php
@@ -1,16 +1,29 @@
-<?php include("Layout/Header.php"); ?>
-
+<?php
+function my_autoloader($class) {
+    include 'Classes/' . $class . '.php';
+}?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/Main.css">
+    <title>VVZA Portaal</title>
+</head>
+<body>
 <nav>
     <div class="links">
       <i class="fas fa-apple-alt"></i>
-      <p>Full stack</p>
+      <p>VVZA Portaal</p>
     </div>
     <div class="rechts">
     <ul>
       <li>
         <a class="active" href="index.php">Home</a> |  
-        
+        <a class="active" href="./Private/login.php">Login</a>
       </li>
     </ul>
     </div>
   </nav>
+</body>
+</html>
