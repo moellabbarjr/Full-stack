bbb757e04f6d2efc9ff574d9288f7cdddeef8058
Igor Kettlitz <igor.kettlitz19@gmail.com>
2020-12-10 14:31:56 +0100

    Fixed header.php

--- Layout/Header.php
+++ Layout/Header.php
@@ -10,22 +10,24 @@ spl_autoload_register(function($class){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Style/Main.css">
+    <script src="Javascript/script.js"></script>
+    <script src="https://kit.fontawesome.com/f1e6762606.js" crossorigin="anonymous"></script>
     <title>VVZA Portaal</title>
 </head>
 <body>
-<nav>
+  <nav>
     <div class="links">
-      <image src="../Style/igootje.png" class="vvzaIcon"></image>
-      <p class="navTitle">VVZA Portaal</p>
+    <a href="../index.php">
+      <image src="../Style/igootje.png"></image>
+    </a>
+    <p>VVZA Portaal</p>
     </div>
     <div class="rechts">
     <ul>
       <li>
-        <a class="active" href="../index.php">Home</a> |  
-        <a class="active" href="login.php">Login</a>
+        <a class="active" href="../index.php">Home</a>
+        <a class="active" href="../Private/login.php">Login</a>
       </li>
     </ul>
     </div>
   </nav>
-</body>
-</html>
--- index.php
+++ index.php
@@ -32,32 +32,9 @@ function GetWeek() {
   
   return "Week {$week} - {$year}";
 }
+require_once('layout/Header.php') 
 ?>
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Style/Main.css">
-    <script src="Javascript/script.js"></script>
-    <script src="https://kit.fontawesome.com/f1e6762606.js" crossorigin="anonymous"></script>
-    <title>VVZA Portaal</title>
-</head>
-<body>
-  <nav>
-    <div class="links">
-    <image src="Style/igootje.png" class="vvzaIcon"></image>
-    <p>VVZA Portaal</p>
-    </div>
-    <div class="rechts">
-    <ul>
-      <li>
-        <a class="active" href="index.php">Home</a>
-        <a class="active" href="./Private/login.php">Login</a>
-      </li>
-    </ul>
-    </div>
-  </nav>
+
   <div id="calendar">
     <form action="" method="POST">
       <div id="calendar-head">
@@ -110,5 +87,4 @@ function GetWeek() {
       </div>
     </div>
   </div>
-</body>
-</html>
+<?php require_once('layout/Footer.php') ?>
