46bb23d6ae4ce43bb61d93e8861675619608c9a5
Leon van den Heuvel <leonheuveltje@gmail.com>
2020-12-10 14:05:13 +0100

    added database functionality to calendar

---
+++ Applicatie/Classes/Calendar.php
@@ -0,0 +1,77 @@
+<?php
+include 'Classes/DB.php'; // pliz tell me how da autoloader works
+
+class Calendar
+{
+    function GetWeek() {
+        $offset = $_SESSION["week_offset"];
+
+        $week = intval(date("W", strtotime("{$offset} week")));
+        $year = date("o", strtotime("{$offset} week")); // o haalt het jaar van de week. Y haalt het huidige jaar. We moeten nog bespreken welke we willen.
+        
+        return "Week {$week} - {$year}";
+    }
+
+    function GetTasks() {
+        $html = "";
+
+        $offset = $_SESSION["week_offset"];
+
+        $week = intval(date("W", strtotime("{$offset} week")));
+
+        $conn = (new DB)->connect();
+        $sql = "SELECT `a`.`id`, `u`.`first_name`, `u`.`last_name`, `a`.`created_at`, `j`.`job_title`,`j`.`job_desc` 
+                FROM `agenda` a 
+                LEFT JOIN `job` j ON `a`.`job_id` = `j`.`job_id` 
+                LEFT JOIN `user` u ON `a`.`user_id` = `u`.`user_id`
+                WHERE WEEK(`a`.`created_at`, 3) = ?
+                ORDER BY `a`.`created_at` ASC";
+        
+        $stmt= $conn->prepare($sql);
+        $stmt->execute([$week]);
+        
+        
+
+        if ($stmt->rowCount() > 0) {
+            $result = $stmt->fetchAll();
+
+            foreach ($result as $row) {
+                $id = $row["id"];
+                $first_name = $row["first_name"];
+                $last_name = $row["last_name"];
+                // $date = date("l j F", strtotime($row["created_at"]));
+                $date = strftime("%A %e %B", strtotime($row["created_at"]));
+                $job = $row["job_title"];
+                $description = $row["job_desc"];
+    
+    
+                $html .= <<<HTML
+                <div class="calendar-item">
+                    <div class="item-head">
+                        <div class="item-name">$first_name $last_name</div>
+                        <div class="item-date">$date</div>
+                    </div>
+    
+                    <div class="item-body">
+                        $description
+                    </div>
+    
+                    <div class="item-foot">
+                        <div>
+                            <label for="done$id">Gedaan:</label>
+                            <input type="checkbox" name="done$id" id="done$id" value="$id">
+                        </div>
+                    </div>
+                </div>
+HTML;
+            }
+        }
+        else {
+            $html = <<<HTML
+            <h2 class="item-message">Geen afspraken voor deze week</h2>
+HTML;
+        }
+
+        return $html;
+    }
+}
--- Applicatie/index.php
+++ Applicatie/index.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
 date_default_timezone_set("Europe/Amsterdam");
+setlocale(LC_TIME, 'Dutch');
 
 function my_autoloader($class) {
     include 'Classes/' . $class . '.php';
 }
+include 'Classes/Calendar.php'; // autoloader ish broken :(
+
+$calendar = new Calendar();
 
 if (!isset($_SESSION["week_offset"])) {
   $_SESSION["week_offset"] = 0;
@@ -20,17 +24,6 @@ if (isset($_POST["next"])) {
 
 if (isset($_POST["reset"])) {
   $_SESSION["week_offset"] = 0;
-}
-
-$query = "";
-
-function GetWeek() {
-  $offset = $_SESSION["week_offset"];
-
-  $week = intval(date("W", strtotime("{$offset} week")));
-  $year = date("o", strtotime("{$offset} week")); // o haalt het jaar van de week. Y haalt het huidige jaar. We moeten nog bespreken welke we willen.
-  
-  return "Week {$week} - {$year}";
 }
 ?>
 <!DOCTYPE html>
@@ -64,8 +57,8 @@ function GetWeek() {
         <button type="submit" name="previous" class="calendar-arrow"><i class="fas fa-arrow-left"></i></button>
 
         <div id="calendar-week">
-          <?=GetWeek()?>
-          <button type="submit" name="reset" class="calendar-button"><i class="fas fa-redo"></i></button>
+          <?=$calendar->GetWeek()?>
+          <button type="submit" name="reset" class="calendar-button"><i class="fas fa-calendar-check"></i></button>
         </div>
 
         <button type="submit" name="next" class="calendar-arrow"><i class="fas fa-arrow-right"></i></button>
@@ -73,41 +66,7 @@ function GetWeek() {
     </form>
 
     <div id="calendar-body">
-      <div class="calendar-item">
-        <div class="item-head">
-          <div class="item-name">Jan</div>
-          <div class="item-date">Maandag 6 januari 2020</div>
-        </div>
-
-        <div class="item-body">
-          Vloer vegen
-        </div>
-
-        <div class="item-foot">
-          <div>
-            <label for="done1">Gedaan:</label>
-            <input type="checkbox" name="done2" id="done1">
-          </div>
-        </div>
-      </div>
-
-      <div class="calendar-item">
-        <div class="item-head">
-          <div class="item-name">Kees</div>
-          <div class="item-date">Dinsdag 7 januari 2020</div>
-        </div>
-
-        <div class="item-body">
-          Tafels schoonmaken
-        </div>
-
-        <div class="item-foot">
-          <div>
-            <label for="done2">Gedaan:</label>
-            <input type="checkbox" name="done2" id="done2">
-          </div>
-        </div>
-      </div>
+      <?=$calendar->GetTasks()?>
     </div>
   </div>
 </body>
