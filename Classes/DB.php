47a4840940a3d00ee2461234a073c6dc5413f77c
Ayrton <510803@edu.rocmn.nl>
2021-01-05 10:35:23 +0100

    applicatie

--- Classes/DB.php
+++ Classes/DB.php
@@ -5,7 +5,7 @@ class DB
 
     $dns = 'mysql:host=127.0.0.1;dbname=vvza_database';
     $user = 'root';
-    $pass = 'root';
+    $pass = '';
 
     return new PDO($dns, $user, $pass);
     }
---
+++ Classes/Job.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shaqu
+ * Date: 10-12-2020
+ * Time: 13:45
+ */
+
+class Job
+{
+    public function getJob()
+    {
+        try {
+            $conn = (new DB)->connect();
+
+            $stmt = $conn->query("SELECT job_title FROM job");
+
+            $result = $stmt->fetchAll();
+            $connection = null;
+            return $result;
+
+        } catch (PDOxception $e) {
+            echo json_encode([
+                'error' => $e->getMessage(),
+            ]);
+
+            print "Error!: " . $e->getMessage() . "<br/>";
+        }
+
+        exit;
+    }
+}
