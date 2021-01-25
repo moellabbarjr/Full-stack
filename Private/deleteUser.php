8293ee31a20035d95f44fe480843d55ebdd68ada
Max <maxridderikhoff@outlook.com>
2021-01-25 14:34:59 +0100

    Fixed bug to hide the add button on the agenda

--- Private/deleteUser.php
+++ Private/deleteUser.php
@@ -6,13 +6,13 @@ switch($_SESSION['role']){
         echo("Er is iets fout gegaan met het inloggen, probeer het opnieuw. U word over 5 seconden terug gestuurd.");
         header("refresh:6;url=login.php");
         break;
-    case "user":
-    case "admin":
+    case "1":
+    case "2":
         $deny = true;
         echo("Uw heeft niet de rechten om dit te zien, over 5 seconden word u teruggestuurd naar uw dashboard.");
         header("refresh:5;url=dashboard.php");
         break;
-    case "super-admin":
+    case "3":
         $user = (new User)->deleteUser($_GET['user_id']);
         header("location:users.php");
 
